a2bf1f26281b725459833352ee49d0475d0d9c76
= <jzw2@illinois.edu>
2022-02-12 17:37:21 -0600

    Still can't find the header

--- default.nix
+++ default.nix
@@ -15,48 +15,55 @@ stdenv.mkDerivation {
     irrlicht
     libGLU
     lua5_4
-    # (premake5.overrideAttrs (old: rec {
-    #     version = "5.0.0-beta1";
+    (premake5.overrideAttrs (old: rec {
+        version = "5.0.0-alpha15";
 
-    #     src = fetchFromGitHub {
-    #       owner = "premake";
-    #       repo = "premake-core";
-    #       rev = "v${version}";
-    #       sha256 = "sha256-8D9032JLAog1PvvsBODVlqFoBy86z9z2L4I9GlTbPw4=";
-    #     };
-    #   }))
+        src = fetchFromGitHub {
+          owner = "premake";
+          repo = "premake-core";
+          rev = "v${version}";
+          # sha256 = "0000000000000000000000000000000000000000000000000000";
+          # 12
+          # sha256 = "sha256-7mN7zhJ0++YO2xq1E0APskzwWaMCvEWstCT9dk3KcMA=";
+          #
+          # 14
+          # sha256 = "sha256-qwIE9qLE0hLNsZPhDrS1+BzwE3nZi8wcQ1AGS88SlrI=";
+          #
+          # 15
+          sha256 = "sha256-8Pdhqwek8zWLcahMgV+fdbsU3tA9oOnefvn6vvBhvDE=";
+        };
+      }))
   ];
 
-  buildPhase = ''
-echo "setting a bunch of dumb variables"
-export DEPENDENCIES_BASE_URL=https://github.com/edo9300/edopro-vcpkg-cache/releases/download/20220210
-export BUILD_CONFIG=release
 
+VCPKG_ROOT= "$PWD/vcpkg";
 
-export DEPLOY_BRANCH="travis-linux"
-export TRAVIS_OS_NAME=linux
+buildPhase =
 
-
-export VCPKG_ROOT=$PWD/vcpkg
-export VCPKG_CACHE_7Z_URL=$DEPENDENCIES_BASE_URL/installed_x64-linux.7z
-
-
-echo "patching shebangs"
-patchShebangs ./travis/*
-
-bash ./travis/install-premake5.sh
-
-
-bash ./travis/dependencies.sh
-
-
-
-bash ./travis/build.sh
-bash ./travis/predeploy.sh
-
-
-bash ./travis/predeploy.sh
-  '';
+let cacheFile =
+  let
+DEPENDENCIES_BASE_URL = "https://github.com/edo9300/edopro-vcpkg-cache/releases/download/20220210";
+VCPKG_CACHE_7Z_URL = "${DEPENDENCIES_BASE_URL}/installed_x64-linux.7z";
+    in
+  fetchurl {
+  url = VCPKG_CACHE_7Z_URL;
+  # sha256 = "1111111111111111111111111111111111111111111111111111";
+  sha256 = "sha256-eKGDD3qQobX4pjDL5Oxta+Tw4h2w0tS0ckgAS40152M=";
+};
+    in
+  ''
+echo "my files are currently"
+ls
+mkdir -p "$VCPKG_ROOT"
+cd "$VCPKG_ROOT"
+7z x ${cacheFile}
+cd -
+echo "Finished extraction"
+echo "my files are currently"
+ls
+premake5 gmake2 --vcpkg-root=$VCPKG_ROOT
+make -Cbuild -j2 config=release ygoprodll
+'';
 
   installPhase = ''
     mkdir -p $out/bin
